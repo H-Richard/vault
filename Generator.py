0f407756d835dc2c3b9721f8a7968eeb17282aea
Richard <44233784+H-Richard@users.noreply.github.com>
2018-11-27 15:02:24 -0500

    Add files via upload

--- Generator.py
+++ Generator.py
@@ -1,34 +1,11 @@
-#!/usr/bin/env python
 import random
 import string
-from typing import TextIO
 
-backup_file = open('backup.txt', 'w')
 
-pw = {}
-
-def creation(user: str, usage: str, length: int, storage: dict) -> str:
+def gen(length: int) -> str:
     password = ''
-    if usage in storage:
-        print('Password usage already exists!')
-        return
-    while len(password) < length:
+    while len(password) < int(length):
         password += random.choice(string.ascii_letters + string.digits + string.punctuation)
-    temp = (user + ':' + password)
-    storage[usage] = temp
     return password
 
 
-def backup(key: str, storage:dict, file:TextIO) -> None:
-    for keys in storage:
-        if keys == key:
-            file.write(key + ';' + storage[key])
-
-
-def clear(file: TextIO) -> None:
-    if input('Are you sure you want to clear the backup? -Y/crN') == 'Y':
-        open(file, 'w').close()
-
-
-def retrieve(storage: dict, key: str) -> str:
-    return storage[key]
